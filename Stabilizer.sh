a33a2b8738ffe91926720e56257a4863ff61cde7
alfrzy10 <89910841+alfrzy10@users.noreply.github.com>
2021-12-23 10:24:07 +0700

    Add files via upload

---
+++ Stabilizer.sh
@@ -0,0 +1,803 @@
+echo "wait... "
+echo -e "\n"
+    bar=" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+    barlength=${#bar}
+    i=0
+    while((i<100)); do
+        n=$((i*barlength / 100))
+        printf "\e[00;32m\r[%-${barlength}s]\e[00m" "${bar:0:n}"
+        ((i += RANDOM%5+2))
+        sleep 0.2
+   done
+clear 
+echo ""
+echo ""
+echo "            _,.-------.,_ "
+sleep 0.03
+echo "        ,;~'             '~;, "
+sleep 0.03
+echo "      ,;                     ;, "
+sleep 0.03
+echo "     ;                         ; "
+sleep 0.03
+echo "    ,'                         ', "
+sleep 0.03
+echo "   ,;                           ;, "
+sleep 0.03
+echo "   ; ;      .           .      ; ; "
+sleep 0.03
+echo "   | ;   ______       ______   ; | "
+sleep 0.03
+echo "   |  '/~'     ~' . '~     '~\'  | "
+sleep 0.03
+echo "   |  ~  ,-~~~^~, | ,~^~~~-,  ~  | "
+sleep 0.03
+echo "    |   |        }:{        |   | "
+sleep 0.03
+echo "    |   l       / | \       !   | "
+sleep 0.03
+echo "    .~  (__,.--' .^. '--.,__)  ~. "
+sleep 0.03
+echo "    |     ---;' / | \ ';---     | "
+sleep 0.03
+echo "     \__.       \/^\/       .__/ "
+sleep 0.03
+echo "      V| \                 / |V "
+sleep 0.03
+echo "       | |T~\___!___!___/~T| | "
+sleep 0.03
+echo "       | |'IIII_I_I_I_IIII'| | "
+sleep 0.03
+echo "       |  \,III I I I III,/  | "
+sleep 0.03
+echo "        \   '~~~~~~~~~~'    / "
+sleep 0.03
+echo "          \   .       .   / "
+sleep 0.03
+echo "            \.    ^    ./ " 
+sleep 0.03
+echo "              ^~~~^~~~^  "
+sleep 0.03
+echo ""
+sleep 0.03
+echo "========================================="
+sleep 0.03
+echo "                WELCOME                  "
+sleep 0.03
+echo "========================================="
+sleep 0.03
+echo 
+echo
+echo "Wait...."
+sleep 3
+echo "Trying to"
+echo "[+] Stabilizing Device"
+sleep 3
+echo "[+] Stabilizing Provider"
+sleep 3
+echo "[+] Stabilizing Signal"
+echo ""
+echo "Wait..."
+sleep 0.03
+echo "Preparing data "
+sleep 0.03
+echo "Preparing data "
+sleep 0.03
+echo "Preparing data "
+sleep 0.03
+echo "Preparing data "
+sleep 0.03
+echo "Preparing data "
+sleep 0.03
+echo "Preparing data "
+sleep 0.03
+echo "Preparing data "
+sleep 0.03
+echo "Preparing data "
+sleep 0.03
+echo "Preparing data "
+sleep 0.03
+echo "Preparing data "
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 0.03
+echo "Preparing data"
+sleep 5
+clear 
+echo
+ echo -e "\n"
+    bar=" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+    barlength=${#bar}
+    i=0
+    while((i<100)); do
+        n=$((i*barlength / 100))
+        printf "\e[00;32m\r[%-${barlength}s]\e[00m" "${bar:0:n}"
+        ((i += RANDOM%5+2))
+        sleep 0.2
+   done
+clear 
+echo -e "\n"
+    bar=" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+    barlength=${#bar}
+    i=0
+    while((i<100)); do
+        n=$((i*barlength / 100))
+        printf "\e[00;32m\r[%-${barlength}s]\e[00m" "${bar:0:n}"
+        ((i += RANDOM%5+2))
+        sleep 0.2
+   done
+   clear 
+
+   echo -e "\n"
+    bar=" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+    barlength=${#bar}
+    i=0
+    while((i<100)); do
+        n=$((i*barlength / 100))
+        printf "\e[00;32m\r[%-${barlength}s]\e[00m" "${bar:0:n}"
+        ((i += RANDOM%5+2))
+        sleep 0.2
+   done
+clear 
+echo 
+echo "            _,.-------.,_ "
+sleep 0.03
+echo "        ,;~'             '~;, "
+sleep 0.03
+echo "      ,;                     ;, "
+sleep 0.03
+echo "     ;                         ; "
+sleep 0.03
+echo "    ,'                         ', "
+sleep 0.03
+echo "   ,;                           ;, "
+sleep 0.03
+echo "   ; ;      .           .      ; ; "
+sleep 0.03
+echo "   | ;   ______       ______   ; | "
+sleep 0.03
+echo "   |  '/~'     ~' . '~     '~\'  | "
+sleep 0.03
+echo "   |  ~  ,-~~~^~, | ,~^~~~-,  ~  | "
+sleep 0.03
+echo "    |   |        }:{        |   | "
+sleep 0.03
+echo "    |   l       / | \       !   | "
+sleep 0.03
+echo "    .~  (__,.--' .^. '--.,__)  ~. "
+sleep 0.03
+echo "    |     ---;' / | \ ';---     | "
+sleep 0.03
+echo "     \__.       \/^\/       .__/ "
+sleep 0.03
+echo "      V| \                 / |V "
+sleep 0.03
+echo "       | |T~\___!___!___/~T| | "
+sleep 0.03
+echo "       | |'IIII_I_I_I_IIII'| | "
+sleep 0.03
+echo "       |  \,III I I I III,/  | "
+sleep 0.03
+echo "        \   '~~~~~~~~~~'    / "
+sleep 0.03
+echo "          \   .       .   / "
+sleep 0.03
+echo "            \.    ^    ./ " 
+sleep 0.03
+echo "              ^~~~^~~~^  "
+sleep 0.03
+echo -e "\n"
+    bar=" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+    barlength=${#bar}
+    i=0
+    while((i<100)); do
+        n=$((i*barlength / 100))
+        printf "\e[00;32m\r[%-${barlength}s]\e[00m" "${bar:0:n}"
+        ((i += RANDOM%5+2))
+        sleep 0.2
+   done
+
+echo -e "\n"
+    bar=" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+    barlength=${#bar}
+    i=0
+    while((i<100)); do
+        n=$((i*barlength / 100))
+        printf "\e[00;32m\r[%-${barlength}s]\e[00m" "${bar:0:n}"
+        ((i += RANDOM%5+2))
+        sleep 0.2
+   done
+
+echo -e "\n"
+    bar=" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+    barlength=${#bar}
+    i=0
+    while((i<100)); do
+        n=$((i*barlength / 100))
+        printf "\e[00;32m\r[%-${barlength}s]\e[00m" "${bar:0:n}"
+        ((i += RANDOM%5+2))
+        sleep 0.2
+   done
+clear 
+
+echo ""
+echo "[!]"
+sleep 1
+echo "Now Your Device"
+sleep 1
+echo "is AlReady for "
+sleep 1  
+echo "HACKING"
+echo
+echo 
+echo 
+echo "[!]"
+sleep 0.03
+echo "this tool is original"
+sleep 0.03
+echo "made by Habibie"
+sleep 0.03
+echo "AF/Alfrzy10"
+sleep 10
+clear 
+sleep 0.03
+sleep 5
+echo "JUST KIDDING MAN HEHEHE"
